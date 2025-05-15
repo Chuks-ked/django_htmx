d60b97aa2f097b308242984f29de3aa6b9aa4581
Chuks-ked <ikedichukwubuikem@gmail.com>
2025-05-16 00:22:04 +0100

    logic to create post

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Post
+from .forms import PostCreateForm
 
 # Create your views here.
 
 def home_view(request):
-    return render( request, 'posts/home.html')
+    posts = Post.objects.all()
+    return render(request, 'posts/home.html', {'posts':posts})
+
+
+def post_create_view(request):
+    form = PostCreateForm()
+    if request.method == 'POST':
+        form = PostCreateForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    return render(request, 'posts/post_create.html', {'form':form})
+
