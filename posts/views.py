abad2f3096d365d9e97e4e25a92770cc459c159a
Chuks-ked <ikedichukwubuikem@gmail.com>
2025-05-15 22:54:05 +0100

    title removed

--- posts/views.py
+++ posts/views.py
@@ -3,5 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def home_view(request):
-    title = 'Welcome to Django'
-    return render( request, 'index.html', {'title': title})
+    return render( request, 'posts/home.html')
