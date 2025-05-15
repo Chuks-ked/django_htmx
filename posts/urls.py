cc762dd68b802518aba8de14a88e9efb788db554
Chuks-ked <ikedichukwubuikem@gmail.com>
2025-05-16 00:21:48 +0100

    url for post create

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home_view
+from .views import home_view, post_create_view
 
 
 urlpatterns = [
-    path('', home_view, name='home_view')
+    path('', home_view, name='home'),
+    path('post/create/', post_create_view, name='post-create'),
 ]
