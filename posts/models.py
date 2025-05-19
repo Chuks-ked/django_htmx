740c0a7f07c0db326a3e4d080e427e6e67d3c556
Chuks-ked <ikedichukwubuikem@gmail.com>
2025-05-19 07:33:32 +0100

    tag model

--- posts/models.py
+++ posts/models.py
@@ -10,7 +10,7 @@ class Post(models.Model):
     # author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='posts')
     body = models.TextField()
     # likes = models.ManyToManyField(User, related_name="likedposts", through="LikedPost")
-    # tags = models.ManyToManyField('Tag')
+    tags = models.ManyToManyField('Tag')
     created = models.DateTimeField(auto_now_add=True)
     id = models.CharField(max_length=100, default=uuid.uuid4, unique=True, primary_key=True, editable=False)
     
@@ -22,3 +22,18 @@ class Post(models.Model):
 
     # def get_absolute_url(self):
     #     return f'/post/{self.id}'
+
+class Tag(models.Model):
+    name = models.CharField(max_length=20)
+    # image = models.FileField(upload_to='icons/', null=True, blank=True)
+    slug = models.SlugField(max_length=20, unique=True)
+    # order = models.IntegerField(null=True)
+
+    def __str__(self):
+        return self.name
+    
+    # class Meta:
+    #     ordering = ['order']
+
+    # def get_absolute_url(self):
+    #     return f'/category/{self.slug}/'
