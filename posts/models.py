981066322c015c7d8db9202d863038f566cc6943
Chuks-ked <ikedichukwubuikem@gmail.com>
2025-05-19 11:21:31 +0100

    Tag ordering

--- posts/models.py
+++ posts/models.py
@@ -27,13 +27,13 @@ class Tag(models.Model):
     name = models.CharField(max_length=20)
     # image = models.FileField(upload_to='icons/', null=True, blank=True)
     slug = models.SlugField(max_length=20, unique=True)
-    # order = models.IntegerField(null=True)
+    order = models.IntegerField(null=True)
 
     def __str__(self):
         return self.name
     
-    # class Meta:
-    #     ordering = ['order']
+    class Meta:
+        ordering = ['order']
 
     # def get_absolute_url(self):
     #     return f'/category/{self.slug}/'
