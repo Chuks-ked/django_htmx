fb16f2ce32a0265090c77d9064a91fabb9fc660c
Chuks-ked <ikedichukwubuikem@gmail.com>
2025-05-20 00:07:17 +0100

    more migrations

--- posts/urls.py
+++ posts/urls.py
@@ -4,10 +4,10 @@ from .views import *
 
 urlpatterns = [
     path('', home_view, name='home'),
+    path('category/<tag>/', home_view, name='category'),
     path('post/create/', post_create_view, name='post-create'),
     path('post/delete/<pk>/', post_delete_view, name='post-delete'),
     path('post/edit/<pk>/', post_edit_view, name='post-edit'),
     path('post/<pk>/', post_page_view, name='post'),
-    path('category/<tag>/', category_view, name='category'),
 
 ]
