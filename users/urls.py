641cbd13864232711871e49d080350dd3bc5101f
Chuks-ked <ikedichukwubuikem@gmail.com>
2025-05-21 05:39:20 +0100

    url updated

--- users/urls.py
+++ users/urls.py
@@ -4,5 +4,6 @@ from .views import *
 
 urlpatterns = [
     path('profile/', profile_view, name='profile'),
+    path('<username>/', profile_view, name='userprofile'),
     path('profile/edit/', profile_edit_view, name='profile-edit'),
 ]
