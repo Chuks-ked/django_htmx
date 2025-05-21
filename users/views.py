48f6bda83f5d48fc18a9ea70b135de85b24a931d
Chuks-ked <ikedichukwubuikem@gmail.com>
2025-05-21 07:47:33 +0100

    profile delete voew

--- users/views.py
+++ users/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from django.contrib.auth.models import User
 from django.http import Http404
 from .forms import ProfileForm
@@ -14,7 +17,7 @@ def profile_view(request, username=None):
             raise Http404()
     return render(request, 'users/profile.html', {'profile':profile})
 
-
+@login_required
 def profile_edit_view(request):
     form = ProfileForm(instance=request.user.profile)
 
@@ -25,3 +28,14 @@ def profile_edit_view(request):
             return redirect('profile')
         
     return render(request, 'users/profile_edit.html', {'form':form})
+
+@login_required
+def profile_delete_view(request):
+    user = request.user
+    if request.method == "POST":
+        logout(request)
+        user.delete()
+        messages.success(request, 'Account Deleted')
+        return redirect('home')
+        
+    return render(request, 'users/profile_delete.html', )
