b4780c183429a9d9854d76d57cc95b220f1e685a
Chuks-ked <ikedichukwubuikem@gmail.com>
2025-05-22 18:17:21 +0100

    delete and onboarding url

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,7 @@ from .views import *
 urlpatterns = [
     path('profile/', profile_view, name='profile'),
     path('<username>/', profile_view, name='userprofile'),
+    path('profile/delete/', profile_delete_view, name='profile-delete'),
     path('profile/edit/', profile_edit_view, name='profile-edit'),
+    path('profile/onboarding/', profile_edit_view, name='profile-onboarding'),
 ]
