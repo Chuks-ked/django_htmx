b26d67d11196bc618c1ae6f296b8a56732fb5124
Chuks-ked <ikedichukwubuikem@gmail.com>
2025-05-26 08:46:06 +0100

    comment url added

--- posts/urls.py
+++ posts/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('post/delete/<pk>/', post_delete_view, name='post-delete'),
     path('post/edit/<pk>/', post_edit_view, name='post-edit'),
     path('post/<pk>/', post_page_view, name='post'),
-
+    path('commentsent/<pk>/', comment_sent, name='comment-sent'),
 ]
